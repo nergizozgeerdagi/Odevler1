b12ec6b63e0416b5462585acee10dc9089a0b022
Nergiz Özge ERDAĞI <nergiz.erdagi@std.ankaramedipol.edu.tr>
2021-01-19 17:20:48 +0300

    Proje dosyası ekle.

---
+++ Odevler1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int sayi1;
+        int sayi2 = 150;
+        var result = SayiOut(out sayi1, sayi2);
+        Console.WriteLine(result);
+    }
+    static int SayiOut(out int sayi1, int sayi2)
+    {
+        sayi1 = 100;
+        return sayi1 + sayi2;
+
+    }
+}
